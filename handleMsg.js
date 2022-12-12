0527585be3899b349970cb9fabcebcf9d4214731
HandyCodez <73069246+HandyCodez@users.noreply.github.com>
2022-12-13 07:37:00 +0800

    Update handleMsg.js

--- handleMsg.js
+++ handleMsg.js
@@ -57,68 +57,8 @@ const handleMsg = async (message, client = new Client()) => {
                 break
             }
 
-            case "meme": {
-                if(inputArray.length == 1){
-                    try {
-                        axios
-                        .get("https://candaan-api.vercel.app/api/image/random")
-                        .then((res) => {
-                            const url = res.data.data.url
-                            const source = res.data.data.source
-                            client.sendFile(from, url, "file.pdf");
-                            text = 'source : ' + source
-                        })
-                        .catch((error) => {
-                            console.error(error);
-                            text = error
-                        });
-                    } catch (error) {
-                        text = error
-                    }
-                }else{
-                    text = "perintah salah!"
-                }
-
-                console.log(`${from} has send ${body}`)
-                await client.sendText(from, text)
-                break
-            }
-
         }
     }
 };
 
 module.exports = handleMsg;
-
-            // case 'ai': {
-            //     let input = body
-            //     let inputArray = input.split(" ")
-
-            //     if (inputArray.length == 1) {
-            //         text = "Perintah salah"
-            //     } else {
-            //         inputArray.shift()
-            //         question = inputArray.join(" ")
-
-            //         try {
-            //             const configuration = new Configuration({
-            //                 apiKey: process.env.KEY,
-            //             });
-            //             const openai = new OpenAIApi(configuration);
-            //             const response = await openai.createCompletion({
-            //                 model: "text-davinci-003",
-            //                 prompt: question,
-            //                 max_tokens: 255,
-            //             });
-
-            //             client.sendText(from, response.data.choices[0].text)
-            //         } catch (error) {
-            //             console.log(error)
-            //             text = error
-            //         }
-            //     }
-
-            //     console.log(`${from} has send ${body}`)
-            //     await client.sendText(from, text)
-            //     break
-            // }
